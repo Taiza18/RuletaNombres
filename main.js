ad5e6beebcede62923141662ba101d166def36de
Taiza18 <taizabrito@hotmail.com>
2021-05-04 11:59:37 +0200

    cambios arrays

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 //miArray es array que reemplaza una variable y asi poderle asignar muchos valores o diferentes valores guardados en ella.
-const miArray = ["Alisa","Amanda","Ana G.","Ana l.","Angie","Clau","Daniela R.","Daniela T.","Eliana","Flor","Gaby","Leo","Inga","Pepi","Maria Laura","Marta","Nicole","Olga","Yeraldin","Ranju","Rosangely","Sonia","Taiza","Yasmin"];
-
+let miArray = ["Alisa","Amanda","Ana G.","Ana l.","Angie","Clau","Daniela R.","Daniela T.","Eliana","Flor","Gaby","Leo","Inga","Pepi","Maria Laura","Marta","Nicole","Olga","Yeraldin","Ranju","Rosangely","Sonia","Taiza","Yasmin"];
+let misegundoArray = [];
 const botonhtml = document.getElementById("boton");
 
 function changeName(){
     const random = Math.floor(Math.random() * miArray.length);
     document.getElementById("titulo").innerHTML = miArray[random];
     miArray.splice(random, 1);
+    misegundoArray.push(miArray[random]);
+    if(miArray.length === 0){
+        restart();
+            }
+}
+function restart(){
+   miArray = misegundoArray;
+   misegundoArray = [];
+
 }
 
 botonhtml.addEventListener("click", changeName);
