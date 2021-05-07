540ceff7238f8f2b959564f92c40264803a50f47
Taiza18 <taizabrito@hotmail.com>
2021-05-07 14:05:44 +0200

    cambios olvidados

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//miArray es array que reemplaza una variable y asi poderle asignar muchos valores o diferentes valores guardados en ella.
-let miArray = ["Alisa","Amanda","Ana G.","Ana l.","Angie","Clau","Daniela R.","Daniela T.","Eliana","Flor","Gaby","Leo","Inga","Pepi","Maria Laura","Marta","Nicole","Olga","Yeraldin","Ranju","Rosangely","Sonia","Taiza","Yasmin"];
+
+let listOfNames = ["Alisa","Amanda","Ana G.","Ana l.","Angie","Clau","Daniela R.","Daniela T.","Eliana","Flor","Gaby","Leo","Inga","Pepi","Maria Laura","Marta","Nicole","Olga","Yeraldin","Ranju","Rosangely","Sonia","Taiza","Yasmin"];
 let misegundoArray = [];
 const botonhtml = document.getElementById("boton");
 
@@ -12,19 +12,19 @@ function showChosenNames() {
 }
 
 function changeName() {
-    const random = miArray[Math.floor(Math.random() * miArray.length)];
+    const random = listOfNames[Math.floor(Math.random() * listOfNames.length)];
     document.getElementById("titulo").innerHTML = random;
     console.log(random);
-    miArray.splice(miArray.indexOf(random), 1);
+    listOfNames.splice(listOfNames.indexOf(random), 1);
     addChooseParticipant(random);
     showChosenNames();
-    if(miArray.length ===0){
+    if(listOfNames.length ===0){
         restart();
     }
 }
 
 function restart(){
-    miArray = misegundoArray;
+    listOfNames = misegundoArray;
     misegundoArray = [];
 }
 
@@ -33,6 +33,5 @@ function addChooseParticipant(random) {
     misegundoArray.push(random);
     console.log(misegundoArray);
 }
-
 
 botonhtml.addEventListener("click",changeName);
